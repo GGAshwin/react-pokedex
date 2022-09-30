2bef67ee18be973fefc9936945af319626de1f16
playmaker159 <ashwinprabhu2001@gmail.com>
2022-09-30 09:34:12 +0530

    fixed side effect

--- src/App.js
+++ src/App.js
@@ -10,31 +10,49 @@ function App() {
     img: "",
     id: ""
   })
-  const url = `https://pokeapi.co/api/v2/pokemon/${message}`
-  fetch(url)
-    .then((response) => response.json())
-    .then((data) => {
 
-      // console.log(data.id)
+  React.useEffect(() => {
+    const url = `https://pokeapi.co/api/v2/pokemon/${message}`
+    fetch(url)
+      .then((response) => response.json())
+      .then((data) => {
 
-      setPokedata({
-        name: data.name,
-        img: data.sprites.other.dream_world.front_default,
-        id: data.id
+        console.log(data.id)
+
+        setPokedata({
+          name: data.name,
+          img: data.sprites ? data.sprites.other.dream_world.front_default : "default",
+          id: data.id
+        })
       })
-    })
+  }, [message])
   const handleChange = event => {
     event.preventDefault();
     setMessage(event.target.value);
   };
+  function submit() {
+    const url = `https://pokeapi.co/api/v2/pokemon/${message}`
+    fetch(url)
+      .then((response) => response.json())
+      .then((data) => {
+
+        console.log(data.id)
+
+        setPokedata({
+          name: data.name,
+          img: data.sprites.other.dream_world.front_default,
+          id: data.id
+        })
+      })
+  }
   return (
     <>
       <div className="card">
         <div className="search__bar">
           <input type="text" onChange={handleChange} />
-          {/* <button id="search"><i className="fa-solid fa-magnifying-glass"></i></button> */}
+          <button id="search" onClick={submit}><i className="fa-solid fa-magnifying-glass"></i></button>
         </div>
-        <Card key={pokedata.name} data={pokedata.name} pokeImg={pokedata.img} index={pokedata.id}  />
+        <Card key={pokedata.name} data={pokedata.name} pokeImg={pokedata.img} index={pokedata.id} />
       </div>
     </>
   );
